bae00f12584652b9b4842751bf7041d253c422db
Bagus-BW <bagusbudi.0701@gmail.com>
2024-06-09 17:10:10 +0700

    setup cors

--- app.js
+++ app.js
@@ -8,19 +8,11 @@ var apiRouter = require('./routes/');
 
 var app = express();
 
-const corsWhiteList = ["sehat-scan.netlify.app"]
-
-const corsConfig = {
-    credentials: true,
-    optionSuccessStatus: 200,
-    origin: function (origin, callback) {
-      const isOriginInWhiteList = origin && corsWhiteList.include(origin)
-      callback(null, isOriginInWhiteList)
-    }
-}
-
-app.use(cors(corsConfig));
-app.options("*", cors(corsConfig))
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
