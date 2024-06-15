c64a01a5841ca35d5ae15d082b7a81d6037df32f
Bagus-BW <bagusbudi.0701@gmail.com>
2024-06-15 18:49:46 +0700

    add route find drink

--- routes/api/drink.js
+++ routes/api/drink.js
@@ -4,7 +4,9 @@ const { authJWT } = require("../../app/middlewares");
 const DrinkController = require("../../app/controllers/drink.controller");
 
 router.get("/", authJWT, DrinkController.getListDrink);
+router.get("/:id", authJWT, DrinkController.findDrink);
 router.post("/", authJWT, DrinkController.createDrink);
+router.post("/action", authJWT, DrinkController.favoriteDrink);
 // router.put("/:id", DrinkController.updateFruit);
 router.delete("/:id", authJWT, DrinkController.deleteDrink);
 
