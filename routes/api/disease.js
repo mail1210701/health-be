39007721335b6886aa7783736f6e142276d706c9
Bagus-BW <bagusbudi.0701@gmail.com>
2024-06-15 18:49:35 +0700

    add route find disease

--- routes/api/disease.js
+++ routes/api/disease.js
@@ -4,6 +4,7 @@ const { authJWT } = require("../../app/middlewares");
 const DiseaseController = require("../../app/controllers/disease.controller");
 
 router.get("/", authJWT, DiseaseController.getListDisease);
+router.get("/:id", authJWT, DiseaseController.findDisease);
 router.post("/", authJWT, DiseaseController.createDisease);
 router.put("/:id", authJWT, DiseaseController.updateDisease);
 
